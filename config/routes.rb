d932d5641f179875d30a46dfab8c4b72fd3aec93
qinmingyuan <mingyuan0715@foxmail.com>
2025-11-16 06:13:55 +0800

    fix assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   scope module: :markdown, defaults: { business: 'markdown' } do
     scope '(:base_name)' do
-      resources :assets, only: [:show], constraints: { id: /.+/ }
       resources :posts, only: [:index]
       resources :posts, only: [] do
         collection do
@@ -12,14 +11,18 @@ Rails.application.routes.draw do
         end
       end
     end
+  end
+
+  namespace :markdown, defaults: { business: 'markdown' } do
+    scope '(:base_name)' do
+      resources :assets, only: [:show], constraints: { id: /.+/ }
+    end
     resources :gits, only: [:show] do
       member do
         post '' => :create
       end
     end
-  end
 
-  namespace :markdown, defaults: { business: 'markdown' } do
     namespace :admin, defaults: { namespace: 'admin' } do
       root 'home#index'
       resources :gits do
